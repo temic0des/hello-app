9f249212f7393ebcb867759828683c35a6a9e131
Temi Rahman <topz2k480@proton.me>
2024-09-30 23:40:38 +0200

    Refactoring articles

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 import json, pendulum
 from flask import Flask, render_template
+from slugify import slugify
 
 app = Flask(__name__)
 
 @app.template_filter("humanize")
-def humanize_date(date_string):
+def humanize_date(date_string: str):
 	date = pendulum.parse(date_string)
 	return date.diff_for_humans()
 
+@app.template_filter("slugify")
+def slugify_text(slug):
+	return slugify(slug)
+
 app.jinja_env.filters['humanize_date'] = humanize_date
+app.jinja_env.filters['slugify'] = slugify_text
 
 file_path = "data.json"
 
@@ -31,9 +37,9 @@ def register():
 def login():
 	return render_template('auth/login.html')
 
-@app.route('/news/breaking-news/<slug>')
-def show_news(slug):
-	return render_template('news/index.html')
+@app.route('/articles/breaking-articles/<slug>')
+def show_article(slug):
+	return render_template('articles/index.html')
 
 if __name__ == "__main__":
 	app.run(debug=True)

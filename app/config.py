54d909a4ced927d6ae261f431b3ea5c916cf9358
Temi Rahman <topz2k480@proton.me>
2024-11-15 12:56:23 +0100

    changed config file details

--- app/config.py
+++ app/config.py
@@ -7,5 +7,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
 
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'I-cannot-guess-this-key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
